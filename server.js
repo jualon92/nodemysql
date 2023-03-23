916db8d6bdabfa026c3624160b37a1cf747a33f1
Juan Ignacio Londra <londrajuan@gmail.com>
2023-03-22 21:49:43 -0300

    establish mysql connection

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
+require('dotenv').config();
 const express = require("express")
-const app = express()
-const port = 3000 // deberia pasarse a .env
-
+const app = express() 
+const mysql = require("mysql")
 app.use(express.json());
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+})
+
+connection.connect( (error) => {
+    if (error) throw error
+    console.log("connected")
+})
 
 app.get('/', (req,res) => {
     res.send("hola mundo")
 })
 
-app.listen(port, () => {
-    console.log(`app listening on port port ${port}!`)
+app.listen(process.env.PORT, () => {
+    console.log(`app listening on port port ${process.env.PORT}!`)
 })
 
