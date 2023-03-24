c19520cb32fce21edfa864661a4f3c344577006d
Juan Ignacio Londra <londrajuan@gmail.com>
2023-03-24 01:36:49 -0300

    added index home page

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express, { json } from "express";
-import { createCamion, getCamiones, editCamion, deleteCamion, getCamion } from "./operations.js";
+import {
+  createCamion,
+  getCamiones,
+  editCamion,
+  deleteCamion,
+  getCamion,
+} from "./operations.js";
+import { testDBConnection } from "./Models/Camion.js";
+import path from "path" 
+ 
 
 const app = express();
 app.use(json());
+const __dirname = path.resolve();
+testDBConnection();
+ 
 
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
 
 app.get("/camiones", (req, res) => {
   getCamiones(req, res);
 });
 
 app.get("/camiones/*", (req, res) => {
-    getCamion(req, res);
-  });
-
+  getCamion(req, res);
+});
 
 app.put("/camiones", (req, res) => {
   createCamion(req, res);
