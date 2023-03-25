3f25648129313ea27e55a5aa1a86b04beee776fb
Juan Ignacio Londra <londrajuan@gmail.com>
2023-03-25 01:31:12 -0300

    added alta cliente modal

--- public/src/pages/AltaCliente.tsx
+++ public/src/pages/AltaCliente.tsx
@@ -2,41 +2,42 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { apiMethods, clientesFormHeaders, fetcher } from "../utils"; 
 import { IFormCliente } from "../utils/interfaces";
+
 const AltaCliente = () => {
-  const fetchAddCliente = async (cliente: IFormCliente, e:any) => {
-    try {
-      const response = await fetch(
-        `http://localhost:3000/api/clientes`,
-        clientesFormHeaders(cliente, apiMethods.PUT)
-      );
-      console.log("response", response.status)
-      if (response.status != 200) {
-        throw new Error();
-      }
-
-      e.target.reset()
-      /*
-      sendNotification({
-        msg: `actualizado exitosamente.`,
-        variant: 'success',
-    });*/
-    } catch (err) {
-        console.log("err", err)
-      /*  sendNotification({
-        msg: `Hubo un error al actualizar`,
-        variant: "error",
-      });*/
-    }
-  };
-
-  const submitForm = (e: any) => {
-    e.preventDefault();
-    const nombre = e.target[0].value;
-    const cuit = e.target[1].value;
-    const cliente = { nombre, cuit };
-
-    fetchAddCliente(cliente,e);
-  };
+    const fetchAddCliente = async (cliente: IFormCliente, e:any) => {
+        try {
+          const response = await fetch(
+            `http://localhost:3000/api/clientes`,
+            clientesFormHeaders(cliente, apiMethods.PUT)
+          );
+          console.log("response", response.status)
+          if (response.status != 200) {
+            throw new Error();
+          }
+    
+          e.target.reset()
+          /*
+          sendNotification({
+            msg: `actualizado exitosamente.`,
+            variant: 'success',
+        });*/
+        } catch (err) {
+            console.log("err", err)
+          /*  sendNotification({
+            msg: `Hubo un error al actualizar`,
+            variant: "error",
+          });*/
+        }
+      };
+    
+      const submitForm = (e: any) => {
+        e.preventDefault();
+        const nombre = e.target[0].value;
+        const cuit = e.target[1].value;
+        const cliente = { nombre, cuit };
+    
+        fetchAddCliente(cliente,e);
+      };
 
   return (
     <div className="d-flex justify-content-center pt-5">
--- public/src/pages/Home.tsx
+++ public/src/pages/Home.tsx
@@ -9,21 +9,30 @@ import ProgressBar from "react-bootstrap/ProgressBar";
 import Spinner from "react-bootstrap/Spinner";
 import { Button, Container } from "react-bootstrap";
 import Router from "preact-router";
-
+import AltaClienteModal from "../components/AltaClienteModal"
 const Home = () => {
   const { data, isLoading, mutate } = useSWR(
     "http://localhost:3000/api/clientes",
     fetcher
   );
+  const [modalShow, setModalShow] = React.useState(false);
 
+
+  
   return (
     <>
       <h1>Clientes</h1>
- 
-      <Button variant="primary" href="/clientes"  className=" my-3">
+   
+      
+      <Button variant="primary"   className=" my-3" onClick={() => setModalShow(true)}>
         Alta Cliente
       </Button>{" "}
      
+      <AltaClienteModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+
       {!isLoading && data ? (
         <ClientesTable   clientes={data.message} />
       ) : (
